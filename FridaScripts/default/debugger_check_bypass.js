bd454179304f484ac6b6f2acc670e23b75e085c7
kingking888 <184108270@qq.com>
2020-06-07 17:54:43 +0800

    commit frida_scripts

---
+++ FridaScripts/default/debugger_check_bypass.js
@@ -0,0 +1,9 @@
+//过调试检测
+Java.perform(function () {
+        // send("[Debugger Check Bypass]  Activated");
+        var Debug = Java.use('android.os.Debug');
+        Debug.isDebuggerConnected.implementation = function () {
+            send('[Debugger Check Bypass] isDebuggerConnected() bypassed');
+            return false;
+        }
+});
